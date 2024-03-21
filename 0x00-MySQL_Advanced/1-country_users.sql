af6a01fcfc7b38567b72be9e7b6862b121f69549
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-21 20:36:21 +0300

    table with unique users

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
---In and not out
-
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
