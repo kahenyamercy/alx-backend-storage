297648432fc31cdec6d748c28b3a7952498a1a46
kahenyamercy <kahenyamercy5@gmail.com>
2024-03-29 12:36:44 +0300

    Redis basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,36 +1,80 @@
 #!/usr/bin/env python3
-
-"""
-This module provides a Cache class for interacting with Redis.
-"""
-
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
+from functools import wraps
 
-class Cache:
-    """
-    A class for caching data using Redis.
-    """
 
-    def __init__(self) -> None:
-        """
-        Initialize the Cache class with a Redis client and flush the database.
-        """
+def count_calls(method: Callable) -> Callable:
+    ''' def count calls '''
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        ''' def wrapper '''
+        key_name = method.__qualname__
+        self._redis.incr(key_name, 0) + 1
+        return method(self, *args, **kwds)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    ''' def call history '''
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        ''' def wrapper'''
+        key_m = method.__qualname__
+        inp_m = key_m + ':inputs'
+        outp_m = key_m + ":outputs"
+        data = str(args)
+        self._redis.rpush(inp_m, data)
+        fin = method(self, *args, **kwds)
+        self._redis.rpush(outp_m, str(fin))
+        return fin
+    return wrapper
+
+
+def replay(func: Callable):
+    '''def replay'''
+    r = redis.Redis()
+    key_m = func.__qualname__
+    inp_m = r.lrange("{}:inputs".format(key_m), 0, -1)
+    outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
+    calls_number = len(inp_m)
+    times_str = 'times'
+    if calls_number == 1:
+        times_str = 'time'
+    fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
+    print(fin)
+    for k, v in zip(inp_m, outp_m):
+        fin = '{}(*{}) -> {}'.format(
+            key_m,
+            k.decode('utf-8'),
+            v.decode('utf-8')
+        )
+        print(fin)
+
+
+class Cache():
+    ''' class cache '''
+    def __init__(self):
+        ''' def init '''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Store input data in Redis and return a randomly generated key.
+        ''' def store '''
+        generate = str(uuid.uuid4())
+        self._redis.set(generate, data)
+        return generate
 
-        Args:
-            data: The data to be stored in the cache. It can be a string,
-                  bytes, integer, or float.
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        ''' def get '''
+        value = self._redis.get(key)
+        return value if not fn else fn(value)
 
-        Returns:
-            str: The randomly generated key used to store the data in Redis.
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+    def get_int(self, key):
+        return self.get(key, int)
+
+    def get_str(self, key):
+        value = self._redis.get(key)
+        return value.decode("utf-8")
